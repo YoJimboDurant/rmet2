9bf41eb9bda91a3d1c55ace89fe847d31bde6e5d
unknown <Hzd3@D220880.cdc.gov>
2016-05-16 15:38:28 -0400

    Adding features - need to modify checkTD6405 function to matc checkTD6401 function so local years are captured with these data.

---
+++ R/installAM.R
@@ -0,0 +1,76 @@
+#' @title installAM
+#' 
+#' @description
+#' \code{installAM} Function will download and unzip files from links at \url{http://www3.epa.gov/scram001/metobsdata_procaccprogs.htm}
+#' to a C: drive location. Sets a pathvariable for location of executables. The function also will create "options" for the 
+#' locations of aermet, aerminute, and aersurface executables. If these exist, subsequent functions will utilize them
+#' in deference to locally unzipped programs.
+#' @param rootDir us the directory where you want aermet, aerminute, and aersurface unpacked
+#' @param aermetExists controls if you want to download the binaries again, or if you only want to set options
+#' The first value controls downloading AERMET, the second AERMINUTE, and the third AERSURFACE.  
+#' @examples 
+#' installAM()
+#' @export
+
+
+installAM <- function(rootDir="C:/", aermetExists=rep(FALSE,3)){
+  require(downloader)
+  require(stringr)
+
+  
+  if (R.version$os !="linux-gnu"){
+    setInternet2(use = NA) 
+    setInternet2(use = FALSE) 
+    setInternet2(use = NA) 
+  }
+  
+  
+  #readLines from html to get the latest version of AERMET, AERSURFACE and AERMINUTE
+  
+  scramLines <- readLines("https://www3.epa.gov/scram001/metobsdata_procaccprogs.htm", warn=FALSE)
+  
+  #aerminutedownload
+  aerminuteLink <- grep("https://www.epa.gov/ttn/scram/7thconf/aermod/aerminute_.*?[.]zip", scramLines, value=TRUE)
+  aerminuteLink <- str_extract(aerminuteLink, "https://www.epa.gov/ttn/scram/7thconf/aermod/aerminute_.*?[.]zip")
+  
+  aerminuteVersion <- str_extract(aerminuteLink, "[0-9]{5}")
+  aerminOut <- paste0(rootDir,"aerminute_",aerminuteVersion)
+  
+  programTree <- list(
+    aermetEx = paste0(rootDir,"aermet_exe"),
+    aerminEx = paste0(rootDir,"aerminute_",aerminuteVersion),
+    aerSurfaceEx= paste0(rootDir,"aersurface_exe")
+  )
+  
+  
+    
+  if(!aermetExists[1]){
+    print("downloading aermet")
+    download("http://www3.epa.gov/ttn/scram/7thconf/aermod/aermet_exe.zip", "aermet_exe.zip", mode = "wb")
+    unzip("aermet_exe.zip", exdir=programTree$aermetEx)
+  }
+  
+  
+  if(!aermetExists[2]){
+    print("downloading aerminute")
+    download(aerminuteLink, str_extract(aerminuteLink, "aerminute.*[.]zip"), mode = "wb")
+    unzip(str_extract(aerminuteLink, "aerminute.*[.]zip"), exdir=programTree$aerminEx)
+  }
+  
+  
+  if(!aermetExists[3]){
+    print("downloading aersurface")
+    download("http://www3.epa.gov/ttn/scram/7thconf/aermod/aersurface_exe.zip", "aersurface_exe.zip", mode = "wb")
+    unzip("aersurface_exe.zip", exdir=programTree$aerSurfaceEx)
+  }
+  
+  # check option status for location of aermet, aersurface and aerminute
+  aermet <- getOption("aermet")
+  aerminute <- getOption("aerminute")
+  aersurface <- getOption("aersurface")  
+  
+  options("aermet" = list.files(path = programTree$aermetEx, pattern="aermet.*[.]exe", full.names = TRUE) )
+  options("aerminute" = list.files(path = programTree$aerminEx, pattern="aerminute.*[.]exe", full.names = TRUE) )
+  options("aersurface" = list.files(path = programTree$aerSurfaceEx, pattern="aersurface.*[.]exe", full.names = TRUE) )
+  return(NULL)
+}
