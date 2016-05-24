13864335f05984226006a2729480f0933f368115
unknown <Hzd3@D220880.cdc.gov>
2016-05-24 15:54:07 -0400

    mesoData added w/API

--- R/mesoData.r
+++ R/mesoData.r
@@ -6,10 +6,27 @@
 #' @export
 
 
-mesoData<- function(rmetObj, mesoSite = "http://mesowest.utah.edu/cgi-bin/droman/station_total_text.cgi?stn="){
+mesoData<- function(rmetObj, API_KEY, type=c("surf","ua")){
+  stopifnot(type %in% c("surf", "ua"))
   stopifnot(is(rmetObj) == "rmet")
   if(rmetObj$surf_Call==""| is.na(rmetObj$surf_Call) | is.null(rmetObj$surf_Call)) stop("empty or missing surf_Call")
   
-  mesoSite <- paste0(mesoSite, rmetObj$surf_Call)
-  readLines(mesoSite)
+#   library(RCurl)
+#   library(jsonlite)
+
+  if("surf" %in% type)
+    station <- rmetObj$surf_Call
+    x <- getURL(paste0("http://api.mesowest.net/v2/stations/metadata?&token=",myAPI, "&complete=1&sensorvars=1", "&stids=", station))
+    x <- fromJSON(x)       
+    if(x$SUMMARY$RESPONSE_MESSAGE !="OK") stop("Error retrieving data from Meso West")
+    rmetObj$surf_Latitude  <- as.numeric(x$STATION$LATITUDE[[1]])
+    rmetObj$surf_Longitude <- as.numeric(x$STATION$LONGITUDE[[1]])
+    rmetObj$surf_Elevation <- as.numeric(x$STATION$ELEVATION)[[1]] * 0.3048
+
+    #get station anemometer height
+    noaaSite <- "http://www.nws.noaa.gov/ops2/Surface/documents/windtower.xls"
+    
+  return(rmetObj)
+  
 }
+
